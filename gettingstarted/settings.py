5f23db6fd6e65a7865c4c1fb941f316558b7e5b0
matias.tafernaberry <gonzalo.tafernaberry@gmail.com>
2021-03-23 17:23:45 -0300

    cors para reportes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
+    'https://reports.eyewatch.me/',
+    'https://reports.eyewatch.me/latamuy/',
     'http://127.0.0.1:8000',
     'https://d0cad94259c9.ngrok.io',
     'http://d0cad94259c9.ngrok.io'
