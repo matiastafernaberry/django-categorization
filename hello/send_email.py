a9d06804530e1f8fe97e67043ce1b0e6d4232f09
matias.tafernaberry <gonzalo.tafernaberry@gmail.com>
2021-06-23 16:35:08 -0300

    cambios en send mail

--- hello/send_email.py
+++ hello/send_email.py
@@ -12,6 +12,7 @@ import RAKE
 import jinja2
 import csv
 import smtplib
+import datetime
 from email.message import EmailMessage
 
 from django.shortcuts import render
@@ -29,6 +30,8 @@ class SendEmailClass(View):
 		dataPost = request.body.decode('utf-8')
 		dataPost = json.loads(dataPost)
 		print(dataPost)
+		hora = datetime.datetime.now()
+		hora = hora.strftime("%d-%m-%Y, %H:%M:%S")
 		content = '<table>'
 		content += "<tr><td style='padding:10px;font-weight:700;'>Desde: beenews.hellobee.io</td></tr>"
 		content += "<tr><td style='padding:10px;font-weight:700;'>Nombre: {0}</td></tr>".format(dataPost['nombre'])
@@ -36,13 +39,14 @@ class SendEmailClass(View):
 		content += "<tr><td style='padding:10px;font-weight:700;'>Correo Electronico: {0}</td></tr>".format(dataPost['email'])
 		content += "<tr><td style='padding:10px;font-weight:700;'>Numero de telefono movil:	{0}</td>".format(dataPost['telefono'])
 		content += "<tr><td style='padding:10px;font-weight:700;'>Empresa: {0}</td></tr>".format(dataPost['nombre_empresa'])
-		content += "<tr><td style='padding:10px;font-weight:700;'>Direccion web empresa: {0}</td></tr></table>".format(dataPost['direccion_empresa'])
-		content += "<tr><td style='padding:10px;font-weight:700;'>Datos usuario: {0}</td></tr></table>".format(dataPost['datoUser'])
+		content += "<tr><td style='padding:10px;font-weight:700;'>Direccion web empresa: {0}</td></tr>".format(dataPost['direccion_empresa'])
+		content += "<tr><td style='padding:10px;font-weight:700;'>Datos usuario: {0}</td></tr>".format(dataPost['datoUser'])
+		content += "<tr><td style='padding:10px;font-weight:700;'>Hora: {0}</td></tr></table>".format(hora)
 
 		msg = EmailMessage()
-		msg['Subject'] = ''
-		msg['From'] = "beenews.hellobee.io"
-		msg['To'] = "ask@hellobee.io, mtafernaberry@eyewatch.me" #, gcadeiras@eyewatch.me"#,
+		msg['Subject'] = 'Solicitud de demo de BeeNews'
+		# msg['From'] = "beenews.hellobee.io"
+		msg['To'] = "ask@hellobee.io"
 		msg.add_header('Content-Type','text/html')
 		msg.set_payload(content)
 		# Send the message via our own SMTP server.
