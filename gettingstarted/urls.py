9391008e0c430a244beafd1ec9d32401e4d16a45
matias <gonzalo.tafernaberry@gmail.com>
2022-04-28 20:31:52 -0300

    cambios

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
 	path("", csrf_exempt(FileClass.as_view())),
     path("files/<str:filename>", csrf_exempt(FileDownloadClass.as_view())),
     path("bg-documents-sharedcount/", csrf_exempt(ApiGetDocumentsSharedCount7Class.as_view())),
+    path("bg-documents-7-days-all-by-client/", csrf_exempt(ApiGetBGDocuments7AllByClient.as_view())),
     path("keywordextract/", csrf_exempt(NameExtractClass.as_view())),
     path("nameextract/", csrf_exempt(NameExtractClass.as_view())),
     path("buzztracker/", csrf_exempt(BuzzTrackerClass.as_view())),
